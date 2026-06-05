1288d4cfe2bcd7d297ed515f72d72255266e61e8
Omoi Nelson <nnyachoti@inkomoko.com>
2026-06-05 14:08:57 +0300

    CGLT-615 Update getOffice to use office in m_loan if present else default to client office

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -455,6 +455,10 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
     @Column(name = "generic_loan_counter", nullable = true)
     private Integer genericLoanCounter;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "office_id",nullable = true)
+    private Office office;
+
     public static Loan newIndividualLoanApplication(final String accountNo, final Client client, final Integer loanType,
             final LoanProduct loanProduct, final Fund fund, final Staff officer, final CodeValue loanPurpose,
             final LoanTransactionProcessingStrategy transactionProcessingStrategy,
@@ -470,7 +474,7 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         return new Loan(accountNo, client, group, loanType, fund, officer, loanPurpose, transactionProcessingStrategy, loanProduct,
                 loanRepaymentScheduleDetail, status, loanCharges, collateral, syncDisbursementWithMeeting, fixedEmiAmount,
                 disbursementDetails, maxOutstandingLoanBalance, createStandingInstructionAtDisbursement, isFloatingInterestRate,
-                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department);
+                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department,null);
     }
 
     public static Loan newGroupLoanApplication(final String accountNo, final Group group, final Integer loanType,
@@ -487,7 +491,7 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         return new Loan(accountNo, client, group, loanType, fund, officer, loanPurpose, transactionProcessingStrategy, loanProduct,
                 loanRepaymentScheduleDetail, status, loanCharges, collateral, syncDisbursementWithMeeting, fixedEmiAmount,
                 disbursementDetails, maxOutstandingLoanBalance, createStandingInstructionAtDisbursement, isFloatingInterestRate,
-                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department);
+                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department,null);
     }
 
     public static Loan newIndividualLoanApplicationFromGroup(final String accountNo, final Client client, final Group group,
@@ -503,7 +507,7 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         return new Loan(accountNo, client, group, loanType, fund, officer, loanPurpose, transactionProcessingStrategy, loanProduct,
                 loanRepaymentScheduleDetail, status, loanCharges, collateral, syncDisbursementWithMeeting, fixedEmiAmount,
                 disbursementDetails, maxOutstandingLoanBalance, createStandingInstructionAtDisbursement, isFloatingInterestRate,
-                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department);
+                interestRateDifferential, rates, fixedPrincipalPercentagePerInstallment, department,null);
     }
 
     protected Loan() {
@@ -517,7 +521,7 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
             final BigDecimal fixedEmiAmount, final List<LoanDisbursementDetails> disbursementDetails,
             final BigDecimal maxOutstandingLoanBalance, final Boolean createStandingInstructionAtDisbursement,
             final Boolean isFloatingInterestRate, final BigDecimal interestRateDifferential, final List<Rate> rates,
-            final BigDecimal fixedPrincipalPercentagePerInstallment, final CodeValue department) {
+            final BigDecimal fixedPrincipalPercentagePerInstallment, final CodeValue department,final Office office) {
 
         this.loanRepaymentScheduleDetail = loanRepaymentScheduleDetail;
         this.loanRepaymentScheduleDetail.validateRepaymentPeriodWithGraceSettings();
@@ -582,6 +586,8 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         // Add net get net disbursal amount from charges and principal
         this.netDisbursalAmount = this.approvedPrincipal.subtract(deriveSumTotalOfChargesDueAtDisbursement());
         this.department = department;
+
+        this.office = office;
 
     }
 
@@ -4624,24 +4630,45 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         return glimId;
     }
 
+//    public Long getOfficeId() {
+//        Long officeId = null;
+//        if (this.client != null) {
+//            officeId = this.client.officeId();
+//        } else {
+//            officeId = this.group.officeId();
+//        }
+//        return officeId;
+//    }
+
     public Long getOfficeId() {
-        Long officeId = null;
-        if (this.client != null) {
-            officeId = this.client.officeId();
-        } else {
-            officeId = this.group.officeId();
+        if (this.office != null) {
+            return this.office.getId();
         }
-        return officeId;
+        if (this.client != null) {
+            return this.client.officeId();
+        }
+        return this.group.officeId();
     }
 
+//    public Office getOffice() {
+//        Office office = null;
+//        if (this.client != null) {
+//            office = this.client.getOffice();
+//        } else {
+//            office = this.group.getOffice();
+//        }
+//        return office;
+//    }
+
     public Office getOffice() {
-        Office office = null;
-        if (this.client != null) {
-            office = this.client.getOffice();
-        } else {
-            office = this.group.getOffice();
+        LOG.info("Get office here : {} ",this.office);
+        if (this.office != null) {
+            return this.office;
         }
-        return office;
+        if (this.client != null) {
+            return this.client.getOffice();
+        }
+        return this.group.getOffice();
     }
 
     private Boolean isCashBasedAccountingEnabledOnLoanProduct() {
