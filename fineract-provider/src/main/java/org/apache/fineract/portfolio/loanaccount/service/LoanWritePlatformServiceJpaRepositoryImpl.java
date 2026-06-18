38db14d0cb97d1bcf5f22f3e3dd109e7fedbfe73
nelsonopilo <nelsono@inkomoko.com>
2026-06-18 15:48:53 +0300

    Comment out UUID import in LoanWritePlatformService
    Commented out the import statement for UUID.
    
    Signed-off-by: nelsonopilo <nelsono@inkomoko.com>

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
@@ -38,7 +38,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
+//import java.util.UUID;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
