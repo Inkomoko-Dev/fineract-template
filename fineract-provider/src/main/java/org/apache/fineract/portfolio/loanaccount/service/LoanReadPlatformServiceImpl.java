f6c508890c1b18425c840af07e40722e41584d1e
Felix Mutua <felixm@inkomoko.com>
2026-06-22 10:01:22 +0300

    CGLT-585: Fix typo in disbursal template: use accountNumber for setAccountNumber

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
@@ -931,7 +931,7 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService {
 
 
         loanTransactionData.setPaymentTypeId(paymentTypeId);
-        loanTransactionData.setClientAccountNumber(accountNumber);
+        loanTransactionData.setAccountNumber(accountNumber);
         loanTransactionData.setCheckNumber(checkNumber);
         loanTransactionData.setRoutingCode(routingCode);
         loanTransactionData.setReceiptNumber(receiptNumber);
