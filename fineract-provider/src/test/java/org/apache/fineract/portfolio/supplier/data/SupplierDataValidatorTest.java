ab418db37d2970dffb795c761f82485bd31e9107
Felix Mutua <felixm@inkomoko.com>
2026-07-16 07:32:53 +0300

    CGLT-660: Validate supplier callback payloads.

---
+++ fineract-provider/src/test/java/org/apache/fineract/portfolio/supplier/data/SupplierDataValidatorTest.java
@@ -0,0 +1,106 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.portfolio.supplier.data;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.apache.fineract.infrastructure.core.exception.InvalidJsonException;
+import org.apache.fineract.infrastructure.core.exception.PlatformApiDataValidationException;
+import org.apache.fineract.infrastructure.core.exception.UnsupportedParameterException;
+import org.apache.fineract.infrastructure.core.serialization.FromJsonHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SupplierDataValidatorTest {
+
+    private SupplierDataValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        this.validator = new SupplierDataValidator(new FromJsonHelper());
+    }
+
+    @Test
+    void blankJsonFails() {
+        assertThrows(InvalidJsonException.class, () -> validator.validateForUpsert(""));
+        assertThrows(InvalidJsonException.class, () -> validator.validateForUpsert("   "));
+        assertThrows(InvalidJsonException.class, () -> validator.validateForUpsert(null));
+    }
+
+    @Test
+    void missingRequiredFieldsFail() {
+        assertThrows(PlatformApiDataValidationException.class, () -> validator.validateForUpsert("{}"));
+        assertThrows(PlatformApiDataValidationException.class,
+                () -> validator.validateForUpsert("{\"externalId\":\"SUP-1\",\"name\":\"Abebe\"}"));
+        assertThrows(PlatformApiDataValidationException.class,
+                () -> validator.validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"name\":\"Abebe\"}"));
+        assertThrows(PlatformApiDataValidationException.class,
+                () -> validator.validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-1\"}"));
+    }
+
+    @Test
+    void blankRequiredFieldsFail() {
+        assertThrows(PlatformApiDataValidationException.class, () -> validator
+                .validateForUpsert("{\"sourceSystem\":\"  \",\"externalId\":\"SUP-1\",\"name\":\"Abebe\"}"));
+        assertThrows(PlatformApiDataValidationException.class, () -> validator
+                .validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"\",\"name\":\"Abebe\"}"));
+        assertThrows(PlatformApiDataValidationException.class, () -> validator
+                .validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-1\",\"name\":\"   \"}"));
+    }
+
+    @Test
+    void minimalValidPayloadPasses() {
+        assertDoesNotThrow(() -> validator
+                .validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe Kebede\"}"));
+    }
+
+    @Test
+    void statusOmittedPasses() {
+        assertDoesNotThrow(() -> validator
+                .validateForUpsert("{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe\"}"));
+    }
+
+    @Test
+    void validStatusPasses() {
+        assertDoesNotThrow(() -> validator.validateForUpsert(
+                "{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe\",\"status\":\"ACTIVE\"}"));
+        assertDoesNotThrow(() -> validator.validateForUpsert(
+                "{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe\",\"status\":\"inactive\"}"));
+    }
+
+    @Test
+    void invalidStatusFails() {
+        assertThrows(PlatformApiDataValidationException.class, () -> validator.validateForUpsert(
+                "{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe\",\"status\":\"PENDING\"}"));
+    }
+
+    @Test
+    void overMaxLengthFails() {
+        final String tooLongName = "x".repeat(256);
+        assertThrows(PlatformApiDataValidationException.class, () -> validator.validateForUpsert(
+                "{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"" + tooLongName + "\"}"));
+    }
+
+    @Test
+    void unsupportedParameterFails() {
+        assertThrows(UnsupportedParameterException.class, () -> validator.validateForUpsert(
+                "{\"sourceSystem\":\"KIFIYA\",\"externalId\":\"SUP-001\",\"name\":\"Abebe\",\"unknownField\":1}"));
+    }
+}
