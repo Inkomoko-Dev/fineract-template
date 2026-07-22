ffd45723a2a74434dda33d9a2d6eb2d562ca8e2c
Omoi Nelson <nnyachoti@inkomoko.com>
2026-07-22 11:54:59 +0300

    CGLT-675 Fix double fee charge at disbursement approval request

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/DisbursementRequestServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/DisbursementRequestServiceImpl.java
@@ -355,7 +355,7 @@ public class DisbursementRequestServiceImpl implements DisbursementRequestServic
         final Collection<LoanCharge> loanCharges = loan.getLoanCharges();
 
         for (final LoanCharge loanCharge : loanCharges) {
-            if (loanCharge.isDueAtDisbursement() && loanCharge.isChargePending()) {
+            if (loanCharge.isDueAtDisbursement() && loanCharge.isChargePending() && loanCharge.isActive()) {
                 chargeAmount = chargeAmount.add(loanCharge.amount());
             }
         }
