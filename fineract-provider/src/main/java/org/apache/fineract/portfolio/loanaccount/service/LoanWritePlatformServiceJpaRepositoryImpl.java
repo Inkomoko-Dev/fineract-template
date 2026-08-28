f6a09106f926d1d64525f47d60579275b42fb7f0
Niceta Nduku <niceta@inkomoko.com>
2026-08-28 16:57:31 +0300

    CGLT-730: Added validation to ensure tranche disbursement dates are not after loan maturity

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
@@ -3460,6 +3460,7 @@ public class LoanWritePlatformServiceJpaRepositoryImpl implements LoanWritePlatf
         this.validateForAddAndDeleteTranche(loan);
 
         loan.updateDisbursementDetails(command, actualChanges);
+        loan.validateTrancheDisbursementDatesAreNotAfterMaturity();
 
         if (loan.loanProduct().isDisallowExpectedDisbursements()) {
             if (!loan.getDisbursementDetails().isEmpty()) {
